26edf70941065c7d3dbc4b273a9936de3863cf41
r-acad <81903541+flt-acdesign@users.noreply.github.com>
2021-04-09 19:14:27 +0200

    Update 21_04_08_Low_speed_perfo_v_0.0.1.jl

--- 21_04_08_Low_speed_perfo_v_0.0.1.jl
+++ 21_04_08_Low_speed_perfo_v_0.0.1.jl
@@ -42,10 +42,19 @@ md" Operating Point:       TAS(m/s) $(@bind TAS_op Slider(1:1:340))  Alt(m) $(@b
 md"### Define aircraft parameters and status   "
 
 # ╔═╡ 22aa1e3d-5265-4e35-90bb-b146954efcf5
-md" Wing area in *m^2* (**Sw**) =  $(@bind Sw NumberField(1:1:1000, default=20))     Aircraft maximum lift coefficient (CLmax) =  $(@bind CLmax NumberField(0.1:.1:7, default=2))      "
+md" Wing area in *m^2* (**Sw**) =  $(@bind Sw NumberField(1:10:1000, default=20))     Aircraft maximum lift coefficient (CLmax) =  $(@bind CLmax NumberField(0.1:.1:7, default=2))      "
 
 # ╔═╡ de07547d-4c70-4793-96f2-8dfb2379ac54
 md" Aircraft mass in *kg* (**M**) =  $(@bind Mass NumberField(0.1:10:600000, default=8000))       Aircraft weight in **N** = $(Mass*9.81)"
+
+# ╔═╡ 0b9c9c9d-4116-49b5-bf33-bcbdda5cb88d
+md" CD0 =  $(@bind CD0 NumberField(0.00001:.0001:2, default=.002))       AC/DC = $(CD0*9.81)"
+
+# ╔═╡ 508a4cb4-5b8b-40b2-a8bd-33f761a06281
+md" e =  $(@bind Oswald NumberField(0.1:.1:1.5, default=.85))  Aspect Ratio =  $(@bind AR NumberField(1:1:30, default=10))  "
+
+# ╔═╡ 5ccc2dc6-7a26-4273-9a6d-1cafd1c7accb
+CDi(_CL, _AR, _e) = _CL^2 /(π * _AR * _e);
 
 # ╔═╡ 27007d5a-6f85-4ff4-9185-ab1e0df69eea
 md"### ISA+0 Atmosphere functions"
@@ -58,6 +67,40 @@ md" ρ(h) :  ISA+0 Density in Kg/m^3 as a function of altitude (h) with h in met
 
 # ╔═╡ c755a5f9-8e92-4612-bfa1-ec543cd66d97
 md"EAS(m/s) = $(round(TAS_op*(ρ(Alt_op)/ρ(0))^.5; digits = 1)) ___ EAS(kt) =  $(round(TAS_op*(ρ(Alt_op)/ρ(0))^.5*1.94384; digits = 1))       "
+
+# ╔═╡ 4077c54d-2a6e-4e80-975c-caf3825f1bc1
+Drag_parasitic(_TAS, _alt, _CD0, _Sw) = .5 * ρ(_alt) * _TAS^2 * CD0 * _Sw;
+
+# ╔═╡ b0c37277-a103-4eef-8ec7-544a6a68cb94
+CL(_TAS, _alt, _Sw, _W) = _W / (.5 * ρ(_alt) * _TAS^2 * _Sw);
+
+# ╔═╡ 4116c5b3-64f8-4f31-8040-cc997ba57243
+Drag_induced(_TAS, _alt, _CD0, _Sw, _e, _W, _AR) = 	.5 * ρ(_alt) * _TAS^2 * CDi(CL(_TAS, _alt,  _Sw, _W), _AR, _e) * _Sw;
+
+# ╔═╡ 887e79e9-c63b-4c52-ade5-44a0bcfdfcf8
+begin
+	
+	plot( xticks = 0:1000:11000, yticks = 0:10:100, leg=true, size=(680, 400),grid = (:xy, :olivedrab, :dot, .5, .8)     ) # Initialize plot with some basic parameters
+	
+	# Plotting the data
+	v1 = (20:1:150)   # Define series for x axis (from 0 to 10000 in steps of 500)
+
+	
+	plot!(v1, Drag_parasitic.(v1, Alt_op, CD0, Sw)       , label = "p(h)/p(0) %", linewidth =3)
+		plot!(v1, Drag_induced.(v1, Alt_op, CD0, Sw, Oswald, Mass*9.81, AR)      , label = "p(h)/p(0) %", linewidth =3)
+	
+		plot!(v1, (Drag_parasitic.(v1, Alt_op, CD0, Sw) + Drag_induced.(v1, Alt_op, CD0, Sw, Oswald, Mass*9.81, AR))      , label = "p(h)/p(0) %", linewidth =3)
+	
+	
+	
+	# Final plot attributes
+	xlabel!("TAS (m)")  # Set label for x axis
+	ylabel!("Thrust required")  # Set label for y axis (wrt: "with respect to")
+	title!("Thrust required for steady and level flight")
+	
+	plot!()  # Update plot with all of the above
+	
+end
 
 # ╔═╡ 8ce71cb5-4f4e-4e02-9999-48cadc5a5e59
 md" p(h) :  ISA+0 Pressure in Pa as a function of altitude (h) with h in meters (Troposphere)"
@@ -73,10 +116,11 @@ T(h) = 288.15 -6.5 * h /1000;
 
 # ╔═╡ a57e579f-5c6e-48f6-a390-2d3b7b816372
 begin
+	
 	plot( xticks = 0:1000:11000, yticks = 0:10:100, leg=true, size=(680, 400),grid = (:xy, :olivedrab, :dot, .5, .8)     ) # Initialize plot with some basic parameters
 	
 	# Plotting the data
-	h1 = (1:500:10000)   # Define series for x axis (from 0 to 10000 in steps of 500)
+	h1 = (1:500:11000)   # Define series for x axis (from 0 to 10000 in steps of 500)
 		
 	plot!(h1, p.(h1)./p(0)*100, label = "p(h)/p(0) %", linewidth =3)
 	plot!(h1, ρ.(h1)./ρ(0)*100, label = "ρ(h)/ρ(0) %", linewidth =3)
@@ -88,6 +132,7 @@ begin
 	title!("% (value at altitude / value at MSL) of ISA quantities")
 	
 	plot!()  # Update plot with all of the above
+	
 end
 
 # ╔═╡ 5afceffa-6e23-422e-81ee-4aee76899d93
@@ -102,7 +147,7 @@ md"""
 md" a(h) :  ISA+0 **speed of sound** in m/s as a function of altitude (**h**) with h in meters (Troposphere). Note `√` is written with `\sqrt<tab>`"
 
 # ╔═╡ 418c035e-6342-464c-9f7b-2c47767a1ede
-a(h) = √(1.4*287*T(h));
+a(h) = √(1.4*287*T(h)); 
 
 # ╔═╡ a50e01cb-8585-4f2d-94d6-1fef73a35660
 md" q(TAS, h) :  ISA+0 **Dynamic pressure (q)** in Pa as a function of True Air Speed in m/s and altitude (**h**) with h in meters (Troposphere). "
@@ -116,8 +161,8 @@ begin
 TAS_range = 1:5:360  # Define the range of TAS for the x axis (from 1 to 360 in steps of 5)
 h_range = [1:250:10000...] # Define the range of altitudes for the y axis (frfom 1 to 10000 metres in steps of 250m(
 
-	
-TAS_Vs1g_at_alt = 	((9.81*Mass)./(ρ.(Alt_op)*CLmax*Sw)).^.5
+# Calculation of stall speed at the operating altitude	
+TAS_Vs1g_at_alt = ((9.81*Mass)/(ρ(Alt_op)*CLmax*Sw))^0.5 
 	
 	
 # Initialize plot	
@@ -138,7 +183,7 @@ plot!((1.0.*a.(h_range))  , h_range, label = "M = 1", lw= 3)
 # Draw a circle showing the operating point under study
 scatter!([TAS_op],[Alt_op], label = "Operating Point", ms = 4)	
 # Draw a label on the operating point
-annotate!([TAS_op]  ,[Alt_op+100], Plots.text("OP", 9, :yellow, :left))
+annotate!([TAS_op]  ,[Alt_op+200], Plots.text("OP", 9, :yellow, :left))
 # Draw values of the operating point	
 annotate!([TAS_op]  ,[Alt_op-300], Plots.text("TAS(kt)= "*string(TAS_op), 8, :yellow, :left))	
 annotate!([TAS_op]  ,[Alt_op-600], Plots.text("h(m)= "*string(Alt_op), 8, :yellow, :left))		
@@ -164,7 +209,7 @@ md" M(TAS,h) :  Mach number for a given *True Air Speed (TAS)* in m/s and altitu
 M(TAS, h) = TAS / a(h) ;
 
 # ╔═╡ 5502a5c3-3ea4-452c-9704-e49e6434aa40
-md"TAS(m/s) = $(TAS_op) ___ TAS(kt) = $(round(TAS_op*1.94384; digits = 1)) ___ Mach no =  $(round(M(TAS_op, Alt_op); digits = 2))    "
+md"TAS(m/s) = $(TAS_op) ___ TAS(kt) = $(round(TAS_op*1.94384; digits = 1)) ___ Mach no =  $(round(M(TAS_op, Alt_op); digits = 2))  ___ Altitude (m) =  $(Alt_op)    "
 
 # ╔═╡ Cell order:
 # ╟─caaadf91-6ddd-4933-bf1a-98fb11ab0fec
@@ -176,6 +221,13 @@ md"TAS(m/s) = $(TAS_op) ___ TAS(kt) = $(round(TAS_op*1.94384; digits = 1)) ___ M
 # ╟─d79c73d4-9889-4feb-8eb8-58583dfcc04c
 # ╟─22aa1e3d-5265-4e35-90bb-b146954efcf5
 # ╟─de07547d-4c70-4793-96f2-8dfb2379ac54
+# ╟─0b9c9c9d-4116-49b5-bf33-bcbdda5cb88d
+# ╟─508a4cb4-5b8b-40b2-a8bd-33f761a06281
+# ╟─887e79e9-c63b-4c52-ade5-44a0bcfdfcf8
+# ╟─4077c54d-2a6e-4e80-975c-caf3825f1bc1
+# ╟─4116c5b3-64f8-4f31-8040-cc997ba57243
+# ╟─b0c37277-a103-4eef-8ec7-544a6a68cb94
+# ╟─5ccc2dc6-7a26-4273-9a6d-1cafd1c7accb
 # ╟─27007d5a-6f85-4ff4-9185-ab1e0df69eea
 # ╟─2a6af729-1805-4222-b134-592d25ff1aa5
 # ╠═13b762d4-366a-47a5-ad77-a18e2b187b78
